e383f381b79a26dd067bf376f2afa520faa9610f
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-10-09 00:41:11 +0300

    deleteFunc updated

--- Income_Tracker/src/Vuex/store.js
+++ Income_Tracker/src/Vuex/store.js
@@ -4,24 +4,32 @@ import {createStore} from 'vuex'
 const store = createStore({
 
     state : {
-        balance : ref(0),
-        incomeList : ref([]),
+        balance : 0,
+        incomeList : [],
     },
 
     mutations : {
         addIncomeList (state, payload){
-            const type = payload.radioType.value;
             state.incomeList = [...state.incomeList , payload.list]
-            state.balance = (type === 'Income' ? state.balance + payload.list.amount : state.balance - payload.list.amount);
+            const income = state.incomeList.filter((item) => item.type === 'Income').map((item) => item.amount);
+            const expense = state.incomeList.filter((item) => item.type === 'Expense').map((item) => item.amount);
+            const totalIncome = income.reduce((acc, item) => acc + item, 0);
+            const totalExpense = expense.reduce((acc, item) => acc + item, 0);
+            state.balance = totalIncome - totalExpense;
         },
         deleteIncomeList(state,payload){
-          state.incomeList =  state.incomeList.filter((item) => item.id !== payload)
+            const selected = state.incomeList.filter((item) => item.id === payload);
+            console.log(selected);
+            if(selected[0].type == 'Income'){
+                console.log('income');
+                state.balance = state.balance - selected[0].amount}
+            else{
+                console.log('expense');
+                state.balance = state.balance + selected[0].amount}
+            state.incomeList =  state.incomeList.filter((item) => item.id !== payload)
 
         }
-    }
-
-
-
+    },
 })
 
 export default store
